7d8cd5a3b18c3b120a08687cc7532edb1da5216d
Leonam Knupp <leonamknupp@hotmail.com>
2024-07-06 14:25:38 -0300

    add: include initial files to the project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # earth-defenders
+
 Desenvolvimento de um jogo baseado no clássico jogo Space Invaders para a disciplina de Sistemas Multimídia do Curso de Ciência da Computação no Instituto Federal de Brasília - Campus Taguatinga
---
+++ src/main.js
@@ -0,0 +1,34 @@
+import Phaser from './lib/phaser.js';
+
+const config = {
+    type: Phaser.AUTO,
+    scale: {
+        parent: 'game-container',
+        // width: 1920,
+        // height: 1080,
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    parent: 'game-container',
+    backgroundColor: '#000000',
+    roundPixels: true,
+    render: {
+        pixelArt: true,
+        antialias: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { 
+                y: 0 ,
+                x: 0,
+            },
+            debug: false
+        }
+    },
+        
+}
+
+const game =  new Phaser.Game(config);
+
+
