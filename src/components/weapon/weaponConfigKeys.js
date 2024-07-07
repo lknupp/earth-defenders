63ac189c91ab57e71c899cf25f852c6137539f81
Leonam Knupp <leonamknupp@hotmail.com>
2024-07-07 16:55:05 -0300

    add: config file for weapons

---
+++ src/components/weapon/weaponConfigKeys.js
@@ -0,0 +1,7 @@
+export const CANNON_F_PATH = Object.freeze({
+    CANNON_01: "assets/weapon/ship_01/cannon.png",
+});
+
+export const CANNON_FRAME_DIMENSION = Object.freeze({
+    CANNON_01: { frameWidth: 22, frameHeight: 60}
+});
