7b862e644a6d38a568228bd406e5e7b033e1bb8b
Leonam Knupp <leonamknupp@hotmail.com>
2024-07-09 08:00:16 -0300

    add: create enemy group class

---
+++ src/characters/enemies/enemyGoup.js
@@ -0,0 +1,39 @@
+export default class EnemyGroup extends Phaser.Physics.Arcade.Group {
+    _scene = null;
+    _enemy = null;
+
+    /**
+     * @param {Phaser.Scene} scene
+     * @param {Array} enemy
+     * @description Create a group of enemies
+     * @example
+     * new EnemyGroup(this, enemy);
+     */
+    constructor(scene, enemy) {
+        super(scene.physics.world, scene);
+        this._scene = scene;
+        this._enemy = enemy;
+
+        this.createMultiple({
+            frameQuantity: 30,
+            key: this._enemy,
+            active: false,
+            visible: false,
+            classType: this._enemy.getClassType()
+        });
+    }
+
+    /**
+     * 
+     * @param {number} x 
+     * @param {number} y 
+     */
+    spawnEnemy(x, y) {
+        const enemy = this.getFirstDead(false);
+
+        if (enemy) {
+            enemy.spawn(x, y);
+        }
+    }       
+
+}
